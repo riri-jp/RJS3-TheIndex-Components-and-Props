0bf75114880ecd1a5f4da9fa0045e934f85eaa3c
riri-jp <alahmary.mashari@gmail.com>
2019-03-06 13:34:27 +0300

    List of Authors

---
+++ src/AuthorList.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import AuthorCard from "./AuthorCard";
+import authors from "./data";
+
+class AuthorList extends Component {
+  render() {
+    let authorsList = this.props.authors.map(author => (
+      <AuthorCard key={author.first_name} person={author} />
+    ));
+    return (
+      <div className="authors">
+        <h3>Authors</h3>
+        <div className="row">{authorsList}</div>
+      </div>
+    );
+  }
+}
+
+export default AuthorList;
